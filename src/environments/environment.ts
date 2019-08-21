f7706493c27ddfb84cee603d05587645896e655b
Ivan Sisak <ivo.sisak@gmail.com>
2019-08-21 22:54:11 +0200

    Bug #221 - logo loaded from project location

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   iframeBaseUrl: 'http://static-web.local/',
   metricsBaseUrl: 'http://localhost:3000/api',
-  logoUrl: 'http://pluspng.com/img-png/dhl-png-file-dhl-logo-svg-1280.png'
+  logoUrl: '/assets/logo.png'
 };
 
 /*
