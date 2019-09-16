4fd50f6af689f9fbad6fea11bd4f48eb7efc7069
Ivan Sisak <ivo.sisak@gmail.com>
2019-09-16 21:15:17 +0200

    Bug #257 - sortovanie HostGroup

--- src/app/common/components/sasi-table/group-sort-impl.ts
+++ src/app/common/components/sasi-table/group-sort-impl.ts
@@ -5,12 +5,32 @@ import {SimpleSortImpl} from './simple-sort-impl';
 export class GroupSortImpl extends SimpleSortImpl implements Sort {
   sort(data: SasiGroupRow[], column: SasiColumn, sortType: SasiSortType, sortByRawValue: string) {
     data.forEach(
-      groupRow => groupRow.rows = super.sort(groupRow.rows, column, sortType, sortByRawValue, (row, columnIndex) => row.getCellValue(columnIndex))
+      groupRow => groupRow.rows = super.sort(
+        groupRow.rows,
+        column,
+        sortType,
+        sortByRawValue,
+        (row, columnIndex) => {
+          if (row !== undefined) {
+            return row.getCellValue(columnIndex);
+          }
+          return null;
+        })
     );
     if (column.index === 'name') {
-      return super.sort(data, column, sortType, sortByRawValue, (row, columnIndex) => row.groupRow.getCellValue(columnIndex));
+      return super.sort(data, column, sortType, sortByRawValue, (row, columnIndex) => {
+        if (row !== undefined) {
+          return row.groupRow.getCellValue(columnIndex);
+        }
+        return null;
+      });
     } else {
-      return super.sort(data, column, sortType, sortByRawValue, (row, columnIndex) => row.rows[0].getCellValue(columnIndex));
+      return super.sort(data, column, sortType, sortByRawValue, (row, columnIndex) => {
+        if (row !== undefined && row.rows[0] !== undefined) {
+          return row.rows[0].getCellValue(columnIndex);
+        }
+        return null;
+      });
 
     }
   }
