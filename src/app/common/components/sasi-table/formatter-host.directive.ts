9efcd77d4856a403217eb2f15586ba97980a2323
Ivan Šišák <ivo.sisak@gmail.com>
2020-04-08 13:24:50 +0200

    #349 - CSS grid as layout of tables

--- src/app/common/components/sasi-table/formatter-host.directive.ts
+++ src/app/common/components/sasi-table/formatter-host.directive.ts
@@ -1,7 +1,7 @@
 import {Directive, ViewContainerRef} from '@angular/core';
 
 @Directive({
-  selector: '[appFormatterHost]'
+  selector: '[appFormatterHost]',
 })
 export class FormatterHostDirective {
 
--- src/app/common/components/sasi-table/sasi-table.component.ts
+++ src/app/common/components/sasi-table/sasi-table.component.ts
@@ -309,7 +309,7 @@ export enum SasiSortType {
 
 export const slideInOutAnimation = [ // TODO reuse animation in all collapsed sasi-table group
   trigger('slideInOut', [
-    state('true', style({height: '0px', overflow: 'hidden', margin: '0'})),
+    state('true', style({height: '0px', overflow: 'hidden', margin: '0', display: 'none'})),
     state('false', style({height: '*', overflow: 'hidden', margin: '0'})),
     transition('1 => 0', animate('500ms ease-in')),
     transition('0 => 1', animate('500ms ease-out'))
