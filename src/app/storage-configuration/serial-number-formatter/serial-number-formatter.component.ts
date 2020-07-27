1d32496ded47e9a18dcf968dd9336fe794b6afe8
Ivan Šišák <ivo.sisak@gmail.com>
2020-07-27 21:23:35 +0200

    #403 - fixed click to open form in serial-number-formatter.component.ts

--- src/app/storage-configuration/serial-number-formatter/serial-number-formatter.component.ts
+++ src/app/storage-configuration/serial-number-formatter/serial-number-formatter.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {SystemMetric} from '../../common/models/metrics/system-metric.vo';
 import {SasiColumn, SasiRow} from '../../common/components/sasi-table/sasi-table.component';
+import {FormBusService} from '../form-bus.service';
+import {StorageEntityVo} from '../storage-entity-form/storage-entity-form.component';
 
 @Component({
   selector: 'app-serial-number-formatter',
@@ -14,15 +16,36 @@ export class SerialNumberFormatterComponent implements OnInit {
   @Input() public column: SasiColumn;
   @Input() public rowData: SasiRow;
 
-  constructor() {
+  constructor(private formBus: FormBusService) {
   }
 
   ngOnInit() {
   }
 
+  openForm() {
+    const formData = new StorageEntityVo();
+    formData.serialNumber = this.getCellValue('serialNumber');
+    formData.parentId = this.getCellValue('parentId');
+    formData.prefixReferenceId = this.getCellValue('prefixReferenceId');
+    formData.name = this.getCellValue('name');
+    formData.id = this.getCellValue('id');
+    formData.dkc = this.getCellValue('dkc');
+    formData.room = this.getCellValue('room');
+    formData.rack = this.getCellValue('rack');
+    formData.arrayModel = this.getCellValue('arrayModel');
+    formData.managementIp = this.getCellValue('managementIp');
+    this.formBus.sendFormData(formData);
+  }
+
   getValue() {
     if (this.rowData.cells['serialNumber'] !== undefined || this.rowData.cells['prefixReferenceId'] !== undefined) {
       return [this.rowData.cells['prefixReferenceId'].value, this.rowData.cells['serialNumber'].value].join(' ');
+    }
+    return null;
+  }
+  getCellValue(valueName: string) {
+    if (this.rowData.cells[valueName] !== undefined) {
+      return this.rowData.cells[valueName].value;
     }
     return null;
   }
