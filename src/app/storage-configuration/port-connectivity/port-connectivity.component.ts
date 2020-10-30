131db22005b74498bc91696772dacb6e69cd2bc3
Ivan Šišák <ivo.sisak@gmail.com>
2020-10-30 21:37:48 +0100

    #436 - sorted data in system list dropdown

--- src/app/storage-configuration/port-connectivity/port-connectivity.component.ts
+++ src/app/storage-configuration/port-connectivity/port-connectivity.component.ts
@@ -3,6 +3,7 @@ import {MetricService} from '../../metric.service';
 import {Owner, StorageEntityType} from '../../common/models/dtos/owner.dto';
 import {ExtractStorageEntityUtils} from '../utils/extract-storage-entity.utils';
 import {Observable} from 'rxjs';
+import {SimpleSortImpl} from '../../common/components/sasi-table/simple-sort-impl';
 
 interface StorageEntityList {
   value: number;
@@ -34,7 +35,7 @@ export class PortConnectivityComponent implements OnInit {
   loadData(force: boolean = true) {
 
     this.fetchStorageEntities(StorageEntityType.SYSTEM, null)
-      .subscribe(data => this.systemsList = data);
+      .subscribe(data => this.systemsList = data.sort((a, b) => this.compare(a, b)));
     if (this.selectedSystem != null) {
       this.fetchStorageEntities(StorageEntityType.DKC, this.selectedSystem)
         .subscribe(data => this.dkcList = data);
@@ -64,4 +65,18 @@ export class PortConnectivityComponent implements OnInit {
     return this.systemsList.filter(system => system.id === id);
   }
 
+  compare(a, b) {
+    const avalue = this.getValue(a);
+    const bValue = this.getValue(b);
+    if (avalue > bValue) {
+      return 1;
+    } else if (avalue < bValue) {
+      return -1;
+    }
+    return 0;
+  }
+
+  getValue(a) {
+    return a.detail != null ? a.detail.sortId : null;
+  }
 }
